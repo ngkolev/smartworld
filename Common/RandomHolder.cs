6b9dd7b956e6ec23d7634e902f613df46bb35f2e
ngkolev <nikolay.kolev@icb.bg>
2014-05-17 15:35:54 +0300

    RandomHolder is added to the solution

---
+++ Common/RandomHolder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{ 
+    public static class RandomHolder
+    {
+        public  static readonly Random Random = new Random();
+
+        public static double NextDouble(
+        this Random random,
+        double minValue,
+        double maxValue)
+        {
+            return random.NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+}
