e82940ed2a3e152a3e554a20505b435f3d758421
ngkolev <nikolay.kolev@icb.bg>
2014-05-18 16:04:37 +0300

    Adding relay command to source control

---
+++ SmartWorld.UI/ViewModel/RelayCommand.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SmartWorld.UI.ViewModel
+{
+    public class RelayCommand : ICommand
+    {
+        #region Private Fields
+
+        private Action action;
+        private Func<bool> canExecute;
+
+        #endregion
+
+        #region Constructor
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            this.action = execute;
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action execute)
+            : this(execute, null)
+        {
+
+        }
+
+        #endregion
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute != null)
+                return canExecute();
+            else return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, new EventArgs());
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(null) && action != null)
+            {
+                action();
+            }
+        }
+
+        #endregion
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+        #region Private Fields
+
+        private Action<T> action;
+        private Func<T, bool> canExecute;
+
+        #endregion
+
+        #region Constructor
+
+        public RelayCommand(Action<T> execute, Func<T, bool> canExecute)
+        {
+            this.action = execute;
+            this.canExecute = canExecute;
+        }
+
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+
+        }
+
+        #endregion
+
+        #region ICommand Members
+
+        public bool CanExecute(object parameter)
+        {
+            if (canExecute != null)
+                return canExecute((T)parameter);
+            else return true;
+        }
+
+        public event EventHandler CanExecuteChanged;
+        public void RaiseCanExecuteChanged()
+        {
+            if (CanExecuteChanged != null)
+                CanExecuteChanged(this, new EventArgs());
+        }
+
+        public void Execute(object parameter)
+        {
+            if (CanExecute(null) && action != null)
+            {
+                action((T)parameter);
+            }
+        }
+
+        #endregion
+    }
+}
